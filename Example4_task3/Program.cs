f0d5b3ae4c7ade95d514f883ceb77275b14d070f
денис волков <vdv.duf@mail.ru>
2025-03-05 20:09:47 +0300

    fix

---
+++ Example4_task3/Program.cs
@@ -0,0 +1,22 @@
+﻿namespace Example4_task3
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int a = Convert.ToInt32 (Console.ReadLine());
+            int b = Convert.ToInt32(Console.ReadLine());
+            int n = 1;
+            if (a > 0 && b > 0)
+            {
+                for (int i = 1; i <= b; i++)
+                    n *= a;
+                Console.WriteLine(n);
+            }
+            else 
+            
+                Console.WriteLine("Введите числа больше 0");
+            
+        }
+    }
+}
