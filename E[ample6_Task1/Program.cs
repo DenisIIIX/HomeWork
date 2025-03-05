181a0120f0b8d04670268df6fcc26f38cd810d27
денис волков <vdv.duf@mail.ru>
2025-03-05 20:09:47 +0300

    fix

---
+++ E[ample6_Task1/Program.cs
@@ -0,0 +1,16 @@
+﻿namespace Example6_Task1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введите строку");
+            string str = Console.ReadLine();
+
+            Console.WriteLine("Нап что заменить");
+            string find = Console.ReadLine();
+
+            Console.WriteLine();
+        }
+    }
+}
---
+++ Example4_Task1/Program.cs
@@ -0,0 +1,19 @@
+﻿using System.Runtime.Serialization.Formatters;
+
+namespace Example4_Task1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = Convert.ToInt32(Console.ReadLine());
+
+            int f = 1;
+            for (int i = 1; i<=n; i++)
+            {
+                f *= i;
+                            }
+            Console.WriteLine("{0}",f);
+        }
+    }
+}
