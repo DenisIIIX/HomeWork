94049a8c15b139e3412ac62c94bac2396f8197bc
денис волков <vdv.duf@mail.ru>
2025-03-13 10:18:14 +0300

    fix

---
+++ Example5_Task2_2/Program.cs
@@ -0,0 +1,35 @@
+﻿namespace Example5_Taxk2_2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            const int n = 10;
+            int[] t = new int[n];
+            Random rnd = new Random();
+
+            for (int i = 0; i<n; i++)
+            {
+                t[i] = rnd.Next(-20, 21);
+                Console.Write("{0} ", t[i]);
+            }
+
+            Console.WriteLine();
+
+            int pLus = 0;
+            int mInus = 0;
+            int zero = 0;
+            foreach (int a in t)
+            {
+                if (a == 0)
+                    ++zero;
+                else if (a > 0)
+                    ++pLus;
+                else
+                    ++mInus;
+            }
+            Console.WriteLine("Количество положительных - {0}, количество отрицательных - {1}, количество нулей - {2}", pLus, mInus, zero);
+            Console.ReadKey();
+        }
+    }
+}
---
+++ Example5_Task6_6/Program.cs
@@ -0,0 +1,28 @@
+﻿namespace Example5_Task6_6
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            const int n = 5;
+            int[,] t = new int[n, n];
+
+
+            for (int i = 0; i < n; i++)
+            {
+                for (int j = 0; j < n; j++)
+                {
+                    if ((double)i%2 != 0)
+                        t[i, j] = (double)j%2 != 0 ? 1 : 0;
+                    else
+                        t[i, j] = (double)j%2 == 0 ? 1 : 0;
+
+                    Console.Write("{0} ", t[i, j]);
+                }
+                Console.WriteLine();
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
