857b2b46cbfaa23b2568769258fa4ea75de28317
денис волков <vdv.duf@mail.ru>
2025-03-26 23:26:29 +0300

    fix

---
+++ Example7_Task3/Program.cs
@@ -0,0 +1,43 @@
+﻿namespace Example7_Task3
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            bool reverse = false;
+            const int n = 10;
+            int[] numbers = new int[n];
+            Random rnd = new Random();
+
+
+            for (int i = 0; i<n; i++)
+            {
+                numbers[i] = rnd.Next(0, 51);
+                Console.Write("{0} ", numbers[i]);
+            }
+
+            Console.WriteLine();
+
+            Console.WriteLine("Введите \"1\" для обратного порядка, \"0\" для прямого");
+            int mark = Convert.ToInt32(Console.ReadLine());
+            if (mark == 1)
+                reverse = true;
+
+            PrintNumbers(numbers, reverse);
+
+
+            Console.ReadKey();
+        }
+        static void PrintNumbers(int[] a, bool reverse = false)
+        {
+            int mark = a.Length-1;
+            if (reverse)
+
+                for (int i = mark; i >= 0; i--)
+                    Console.Write("{0,3}", a[i]);
+            else
+                foreach (int element in a)
+                    Console.Write("{0,3}", element);
+        }
+    }
+}
---
+++ Example7_Task5/Program.cs
@@ -0,0 +1,31 @@
+﻿namespace Example7_Task5
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int a1 = 2;
+            int b1 = 3;
+
+            double a2 = 2.5;
+            double b2 = 3.5;
+
+            
+            Console.WriteLine(Multiply(a1, b1));
+
+            double s2 = Multiply(a2, b2);
+            Console.WriteLine(s2);
+            Console.ReadKey();
+        }
+        static int Multiply(int a, int b)
+        {
+            return a * b;
+        }
+
+        static double Multiply(double a, double b)
+        {
+            return a * b;
+        }
+        
+    }
+}
