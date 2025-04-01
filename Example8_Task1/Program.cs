6324066e31cf95c0354a0174998c2fc83b7885b0
денис волков <vdv.duf@mail.ru>
2025-04-02 00:03:24 +0300

    fix

---
+++ Example8_Task1/Program.cs
@@ -0,0 +1,43 @@
+﻿using System.Threading.Channels;
+
+namespace Example8_Task1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                int age = Convert.ToInt32(Console.ReadLine());
+                ValidateAge(age);
+                Console.WriteLine($"Возраст корректен");
+            }
+            catch (FormatException)
+            {
+                Console.WriteLine("Ошибка: Введено не число!");
+            }
+            catch (ArgumentOutOfRangeException ex)
+            {
+                Console.WriteLine($"Ошибка: {ex.Message}");
+            }
+            catch (ArgumentException ex)
+            {
+                Console.WriteLine($"Ошибка: {ex.Message}");
+            }
+            finally
+            {
+                Console.WriteLine("Проверка возраста завершена.");
+            }
+
+            Console.ReadKey();
+        }
+        static void ValidateAge(int a)
+        {
+            if (a < 0)
+                throw new ArgumentException("Возраст не может быть отрицательным");
+            if (a > 150)
+                throw new ArgumentOutOfRangeException("Слишком большой возраст");
+        }
+        
+    }
+}
